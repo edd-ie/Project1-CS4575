1e97530377bc3207f1cbbb322dc2a1dc4d4598c9
edd-ie <ogendoeddie@gmail.com>
2026-03-30 18:34:40 -0300

    MPI grayscale fx()

--- src/ompi.h
+++ src/ompi.h
@@ -1,25 +1,53 @@
 #ifndef OMPI_H
 #define OMPI_H
 
-void mpi_grayscale(unsigned char *const img, unsigned char *const gray_img, const int height, const int width)
+void mpi_grayscale(unsigned char *const img, unsigned char *const gray_img, const int height, const int width, const int size, const int rank)
 {
-    for (int y = 0; y < height; y++)
+    int chunks = width * height;
+    int base_part = chunks / size;
+    int remainder = chunks % size;
+
+    int *counts = (int *)malloc(size * sizeof(int));
+    int *displ = (int *)malloc(size * sizeof(int));
+    int *g_counts = (int *)malloc(size * sizeof(int));
+    int *g_displ = (int *)malloc(size * sizeof(int));
+
+    int offset = 0;
+    int g_offset = 0;
+    for (int i = 0; i < size; i++)
     {
-        for (int x = 0; x < width; x++)
-        {
-            // Find the starting index of the current pixel in the RGBA array
-            int pixel_idx = (y * width + x) * 4;
+        // Ranks get 1 extra pixel
+        int current_chunk_size = base_part + (i < remainder ? 1 : 0);
+        counts[i] = current_chunk_size * 4; // 4 channels rgba
+        displ[i] = offset;
+        offset += counts[i];
 
-            unsigned char r = img[pixel_idx];     // Red
-            unsigned char g = img[pixel_idx + 1]; // Green
-            unsigned char b = img[pixel_idx + 2]; // Blue
-
-            unsigned char gray = (unsigned char)(0.299 * r + 0.587 * g + 0.114 * b);
-
-            // Store in the new 1-channel array
-            gray_img[y * width + x] = gray;
-        }
+        // Use g_counts and g_displ in MPI_Gatherv
+        g_counts[i] = current_chunk_size;
+        g_displ[i] = g_offset;
+        g_offset += g_counts[i];
     }
+
+    // Each rank allocates only what it needs based on its specific count
+    unsigned char *img_buf = (unsigned char *)malloc(counts[rank]);
+    unsigned char *gray_buf = (unsigned char *)malloc(g_counts[rank]);
+
+    MPI_Scatterv(img, counts, displ, MPI_UNSIGNED_CHAR, img_buf, counts[rank], MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+
+    for (int i = 0; i < g_counts[rank]; i++)
+    { // Local index for 4-channel input
+        int in_idx = i * 4;
+        gray_buf[i] = (unsigned char)(0.299 * img_buf[in_idx] + 0.587 * img_buf[in_idx + 1] + 0.114 * img_buf[in_idx + 2]);
+    }
+
+    MPI_Gatherv(gray_buf, g_counts[rank], MPI_UNSIGNED_CHAR, gray_img, g_counts, g_displ, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+
+    free(counts);
+    free(displ);
+    free(g_counts);
+    free(g_displ);
+    free(img_buf);
+    free(gray_buf);
 }
 
 void mpi_gaussian_blur(unsigned char *const in, unsigned char *out, int w, int h)
